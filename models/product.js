afb9349d5017f93e07b2b0a5a28e82bc200825c8
rosane6219 <im_benkhelil@esi.dz>
2020-05-23 00:40:28 +0100

    creating model

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose')
+const Schema = mongoose.Schema
+
+const productSchema = new Schema(
+    {
+        name: String,
+        price: Number
+    }
+)
+ const Product = mongoose.model('product',productSchema)
+
+ module.exports = Product
