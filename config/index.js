142469dd60c7c588c0b8e77836e68e488bb2e45e
rosane6219 <im_benkhelil@esi.dz>
2020-05-23 01:20:29 +0100

    test & changing pwd

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ var configValues = require('./config')
 module.exports = {
     
     getDbConnectionString: function(){
-        return 'mongodb://' + configValues.username + ':' + configValues.pwd + '@localhost:27017/db1'
+        return 'mongodb://' + configValues.username + ':' + configValues.password + '@localhost:27017/db1'
     }
 }
--- test/1_saving_test.js
+++ test/1_saving_test.js
@@ -2,25 +2,21 @@ const assert = require('assert')
 const mongoose = require('mongoose')
 const Product = require('../models/product')
 
-describe('Saving products to database', function(){
-    
-    before(function(done){
-        mongoose.connection.collections.products.drop(function(){
-            mongoose.connection.collections.orders.drop(function(){
-                done()
-            })
-        })
-    })
+describe('Saving tests',function (){
 
     const smartphone0 = new Product({
-        productType: 'Smartphone',
         name: 'Galaxy S20',
-        price: 150000,
+        price: 150000
     })
-    it('Saves products to the database', function(done){
-        Product.collection.insert(smartphone0, function(err){
+
+    it('saves a product',function(done){
+        Product.collection.insertOne(smartphone0, function(err){
             assert(err === null)
             done()
         })
     })
 })
+
+
+
+
